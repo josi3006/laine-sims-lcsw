6d3455a7027778f5609d6d487a1456ded640b2e4
JGSims <josi3006@hotmail.com>
2021-05-28 12:31:56 -0400

    Added Laine portrait to UnderConstruction

--- src/components/underconstruction.js
+++ src/components/underconstruction.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Portrait from "../images/LainePic.jpg"
 
 const UnderConstruction = () => {
 
@@ -23,6 +24,8 @@ const UnderConstruction = () => {
 
                         <p>is currently under construction.</p>
                     </div>
+
+                    {Portrait}
 
 
                     <p>Laine Sims is a Licensed Clinical Social Worker who works with adolescents, adults and families. She has experience and interest in working with individuals and families who are struggling with anxiety, depression, eating disorders, life adjustments, relationship challenges and parenting issues.</p>
